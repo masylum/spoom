65273363698b8ef01c59f7e2df1644b41dc35813
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2020-07-14 18:33:56 -0400

    Add a parser for Sorbet metrics output file
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

---
+++ test/spoom/sorbet/metrics_test.rb
@@ -0,0 +1,74 @@
+# typed: true
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Spoom
+  module Sorbet
+    class MetricsTest < Minitest::Test
+      def test_parses_metrics_error
+        assert_raises JSON::ParserError do
+          Spoom::Sorbet::Metrics.parse_string("")
+        end
+      end
+
+      def test_parses_metrics
+        metrics = Spoom::Sorbet::Metrics.parse_string(<<~ERR)
+          {
+           "repo": "MyRepo",
+           "sha": "1234",
+           "status": "Success",
+           "branch": "master",
+           "timestamp": "1594762766",
+           "uuid": "some-uuid",
+           "metrics": [
+            {
+             "name": "ruby_typer.unknown..error.total",
+             "value": 1
+            },
+            {
+             "name": "ruby_typer.unknown..types.input.sends.total",
+             "value": 2094
+            }
+           ]
+          }
+        ERR
+        assert_equal("MyRepo", metrics.repo)
+        assert_equal("1234", metrics.sha)
+        assert_equal("master", metrics.branch)
+        assert_equal("Success", metrics.status)
+        assert_equal(1, metrics["error.total"])
+        assert_equal(2094, metrics["types.input.sends.total"])
+      end
+
+      def test_parses_metrics_and_removes_prefix
+        metrics = Spoom::Sorbet::Metrics.parse_string(<<~ERR, "metrics.")
+          {
+           "repo": "MyRepo",
+           "sha": "1234",
+           "status": "Success",
+           "branch": "master",
+           "timestamp": "1594762766",
+           "uuid": "some-uuid",
+           "metrics": [
+            {
+             "name": "metrics.error.total",
+             "value": 1
+            },
+            {
+             "name": "metrics.types.input.sends.total",
+             "value": 2094
+            }
+           ]
+          }
+        ERR
+        assert_equal("MyRepo", metrics.repo)
+        assert_equal("1234", metrics.sha)
+        assert_equal("master", metrics.branch)
+        assert_equal("Success", metrics.status)
+        assert_equal(1, metrics["error.total"])
+        assert_equal(2094, metrics["types.input.sends.total"])
+      end
+    end
+  end
+end
